026921cf641bd94b7a8dff8bfaf0771f1b3f3d49
Reece Berens <rberens@ksu.edu>
2017-11-29 12:20:15 -0600

    Update network class

---
+++ FP-Team01/FP-Team01/ClientAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FP_Core;
+
+namespace FP_Team01
+{
+    class ClientAccount : FP_Core.IAccount
+    {
+        private List<IAccount> _contacts;
+        private List<IAccount> _onlineContacts;
+        private string _username;
+        private bool _isOnline = true;
+
+        public List<IAccount> Contacts
+        {
+            get
+            {
+                return _contacts;
+            }
+        }
+
+        public bool IsOnline
+        {
+            get
+            {
+                return _isOnline;
+            }
+        }
+
+        public List<IAccount> OnlineContacts
+        {
+            get
+            {
+                return _onlineContacts;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+        }
+    }
+}
