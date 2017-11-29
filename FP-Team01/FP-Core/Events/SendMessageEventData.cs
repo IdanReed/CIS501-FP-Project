70762df594926e3d69b82cde093ff514f2efbe74
jarodh <jarodh@E1117W05>
2017-11-29 12:33:06 -0600

    Just some chat room changes

---
+++ FP-Team01/FP-Core/Events/SendMessageEventData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP_Core.Events
+{
+    public class SendMessageEventData
+    {
+        public string Username;
+        public string Message;
+        public DateTime Time;
+        public int ChatRoomIndex;
+
+        public SendMessageEventData(string name, string msg, DateTime msgTime, int index)
+        {
+            Username = name;
+            Message = msg;
+            Time = msgTime;
+            ChatRoomIndex = index;
+        }
+
+    }
+}
